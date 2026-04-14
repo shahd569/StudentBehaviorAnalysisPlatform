3f34f6dfd6cb8e2cc16ef2d665a19feebe8af9d6
shahd al haj hasan <shahd345hasan@gmail.com>
2026-04-14 14:25:11 +0300

    student home dashboard route

---
+++ app/api/studentDashboard/homePageCards/route.js
@@ -0,0 +1,122 @@
+import { getServerSession } from "next-auth";
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { authOptions } from "@/lib/auth";
+
+export async function GET() {
+  try {
+    // const session = getServerSession(authOptions);
+    // if (!session || !session.user || session.user.role !== "STUDENT") {
+    //   return NextResponse.json(
+    //     { message: "غير مسموح لك بالوصول لهذه البيانات" },
+    //     { status: 401 },
+    //   );
+    // }
+    // const studentId = parseInt(session.user.id);
+    const studentId = 15;
+    const [
+      activeCoursesCount,
+      allStudentAssignment,
+      studentAssignmentSubmissions,
+      allQuizAttempt,
+      userSessions,
+    ] = await Promise.all([
+      prisma.Enrollment.count({
+        where: {
+          studentId: studentId,
+        },
+        select: { courseId: true },
+      }),
+      prisma.Assignment.findMany({
+        where: { lesson: { courseId: { in: courseIds } } },
+        include: {
+          lesson: {
+            include: {
+              course: true,
+            },
+          },
+        },
+      }),
+      prisma.AssignmentSubmission.findMany({
+        where: { studentId: studentId },
+        select: { assignmentId: true, finalScore: true },
+      }),
+      prisma.QuizAttempt.findMany({
+        where: { studentId: studentId },
+        select: { score: true },
+      }),
+      prisma.UserSession.findMany({
+        where: { studentId: studentId },
+        select: { startTime: true, endTime: true, status: true },
+      }),
+    ]);
+
+    // pending tasks
+    const courseIds = activeCoursesCount.map((e) => e.courseId);
+    const submissionsIds = studentAssignmentSubmissions.map(
+      (a) => a.assignmentId,
+    );
+
+    const filteredAssignments = allStudentAssignment.filter(
+      (a) => !submissionsIds.includes(a.id),
+    );
+    const pendingTasks = filteredAssignments.length;
+    // performance avg
+    const assignmentScoreSum = 0;
+    const assignmentCount = 0;
+    const assignment = studentAssignmentSubmissions.map((a) => {
+      if (a.finalScore != null) {
+        assignmentScoreSum += a.finalScore;
+        assignmentCount++;
+      }
+    });
+    const assignmentAvg = assignmentScoreSum / assignmentCount;
+    const quizScoreSum = 0;
+    const quizCount = 0;
+    const quizzes = allQuizAttempt.map((q) => {
+      if (q.score != null) {
+        quizScoreSum += q.score;
+        quizCount++;
+      }
+    });
+    const quizAvg = quizScoreSum / quizCount;
+    const performanceAvg = (assignmentAvg + quizAvg) / 2;
+
+    // learning hours
+    //  اريد ان يرجع الوقت بالساعات فقط اذا كان بالساعات اما اذا كان اقل من ساعة ف اريده بالدقائق
+    const totalLearningHours = userSessions.reduce((total, session) => {
+      if (session.status === "ENDED") {
+        const duration =
+          (new Date(session.endTime) - new Date(session.startTime)) /
+          (1000 * 60 * 60); // Convert milliseconds to hours
+        return total + duration;
+      }
+      return total;
+    }, 0);
+
+    let learningHours;
+    if (totalLearningHours < 1) {
+      // Return minutes, rounded
+      learningHours = `${Math.round(totalLearningHours * 60)} دقيقة`;
+    } else {
+      // Return hours, rounded to 1 decimal
+      learningHours = `${Number(totalLearningHours.toFixed(1))} ساعة`;
+    }
+
+    return NextResponse.json(
+      {
+        activeCoursesCount: activeCoursesCount,
+        pendingTasks: pendingTasks,
+        performanceAvg: performanceAvg,
+        learningHours: learningHours,
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error fetching home page cards:", error);
+    return NextResponse.json(
+      { message: "حدث خطأ أثناء جلب بيانات الصفحة الرئيسية" },
+      { status: 500 },
+    );
+  }
+}
---
+++ app/api/teacherDashboard/recommendations/route.js
@@ -0,0 +1,34 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { authOptions } from "@/lib/auth";
+
+export async function GET() {
+  try {
+    // const session = await getServerSession(authOptions);
+
+    // if (!session || !session.user || session.user.role !== "STUDENT") {
+    //   return NextResponse.json(
+    //     { message: "غير مسموح لك بالوصول لهذه البيانات" },
+    //     { status: 401 },
+    //   );
+    // }
+
+    // const teacherId = parseInt(session?.user.id);
+    const teacherId = 17;
+
+    const recommendations = await prisma.Recommendation.findMany({
+      where: { teacherId: teacherId },
+      include: {
+        course: true,
+      },
+    });
+    return NextResponse.json({ recommendations });
+  } catch (error) {
+    console.error("Error fetching recommendations:", error);
+    return NextResponse.json(
+      { message: "حدث خطأ أثناء جلب التوصيات" },
+      { status: 500 },
+    );
+  }
+}
